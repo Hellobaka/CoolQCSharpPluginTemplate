6ed61a9dd6205915e4563b88d42b3ff536c72d1c
Mr.喜 <863450594@qq.com>
2024-10-30 11:51:04 +0800

    Initial commit

---
+++ PublicInfos/MainSave.cs
@@ -0,0 +1,33 @@
+using Native.Sdk.Cqp;
+using Native.Tool.IniConfig;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PublicInfos
+{
+    public static class MainSave
+    {
+        /// <summary>
+        /// 保存各种事件的数组
+        /// </summary>
+        public static List<IOrderModel> Instances { get; set; } = new List<IOrderModel>();
+        public static CQLog CQLog { get; set; }
+        public static CQApi CQApi { get; set; }
+        public static string AppDirectory { get; set; }
+        public static string ImageDirectory { get; set; }
+
+        static IniConfig configMain;
+        public static IniConfig ConfigMain
+        {
+            get
+            {
+                if (configMain != null)
+                    return configMain;
+                configMain = new IniConfig(Path.Combine(AppDirectory, "Config.ini"), System.Text.Encoding.UTF8);
+                configMain.Load();
+                return configMain;
+            }
+            set { configMain = value; }
+        }
+    }
+}
